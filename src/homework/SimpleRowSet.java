a74d6ec11150ca0e06526023ca961aa099cd7523
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-12-16 09:09:04 -0800

    add ScrollUpdateRowSet

--- src/homework/SimpleRowSet.java
+++ src/homework/SimpleRowSet.java
@@ -1,30 +1,30 @@
-//package homework;
-//
-//import java.sql.SQLException;
-//import javax.sql.RowSet;
-//import com.sun.rowset.*;
-//
-//
-//public class SimpleRowSet {
-//    public static void main(String[] args)
-//        throws SQLException,ClassNotFoundException{
-//
-//        Class.forName("org.postgresql.Driver");
-//        System.out.println("Driver loaded");
-//
-//        RowSet rowSet = new JdbcRowSetImpl();
-//
-//        rowSet.setUrl("jdbc:postgresql://localhost:5432/javabook");
-//        rowSet.setUsername("postgres");
-//        rowSet.setPassword("postgres");
-//        rowSet.setCommand("select firstName, mi, lastName " +
-//                "from Student where lastName = 'Smith'");
-//        rowSet.execute();
-//
-//        while (rowSet.next())
-//            System.out.println(rowSet.getString(1) + " \t " +
-//                    rowSet.getString(2) + " \t " + rowSet.getString(3));
-//
-//        rowSet.close();
-//    }
-//}
+package homework;
+
+import java.sql.SQLException;
+import javax.sql.RowSet;
+import com.sun.rowset.*;
+
+
+public class SimpleRowSet {
+    public static void main(String[] args)
+        throws SQLException,ClassNotFoundException{
+
+        Class.forName("org.postgresql.Driver");
+        System.out.println("Driver loaded");
+
+        RowSet rowSet = new JdbcRowSetImpl();
+
+        rowSet.setUrl("jdbc:postgresql://localhost:5432/javabook");
+        rowSet.setUsername("postgres");
+        rowSet.setPassword("postgres");
+        rowSet.setCommand("select firstName, mi, lastName " +
+                "from Student where lastName = 'Smith'");
+        rowSet.execute();
+
+        while (rowSet.next())
+            System.out.println(rowSet.getString(1) + " \t " +
+                    rowSet.getString(2) + " \t " + rowSet.getString(3));
+
+        rowSet.close();
+    }
+}
