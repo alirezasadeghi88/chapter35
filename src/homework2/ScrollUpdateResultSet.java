a970c3fb300a6d65154f4d31aed9ba37d1913603
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-12-16 08:52:28 -0800

    add ScrollUpdateResultSet

--- src/homework2/ScrollUpdateResultSet.java
+++ src/homework2/ScrollUpdateResultSet.java
@@ -14,5 +14,10 @@ public class ScrollUpdateResultSet {
                 ("jdbc:postgresql://localhost:5432/javabook2","postgres","postgres");
         connection.setAutoCommit(true);
         System.out.println("Database connected");
+
+        Statement statement = connection.createStatement
+                (ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+
+
     }
 }
