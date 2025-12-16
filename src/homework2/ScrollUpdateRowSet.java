b0bbbfa3b570654f0447ee9d458846fd0ac60f30
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-12-16 09:12:37 -0800

    add ScrollUpdateRowSet

--- src/homework2/ScrollUpdateRowSet.java
+++ src/homework2/ScrollUpdateRowSet.java
@@ -1,5 +1,7 @@
 package homework2;
 
+import javax.sql.RowSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 
 public class ScrollUpdateRowSet {
@@ -8,5 +10,15 @@ public class ScrollUpdateRowSet {
 
         Class.forName("org.postgresql.Driver");
         System.out.println("Driver loaded");
+    }
+
+    private static void displayRowSet(RowSet rowSet)throws SQLException {
+        ResultSetMetaData resultSetMetaData = rowSet.getMetaData();
+        rowSet.beforeFirst();
+        while (rowSet.next()) {
+            for (int i = 1; i <= resultSetMetaData.getColumnCount(); i++)
+                System.out.printf("%-12s\t",rowSet.getObject(i));
+            System.out.println();
+        }
     }
 }
