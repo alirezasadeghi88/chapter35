8a20e4298ac78d6c01aa677baa89b0d171db5036
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-12-16 09:06:09 -0800

    add ScrollUpdateResultSet

--- src/homework2/ScrollUpdateResultSet.java
+++ src/homework2/ScrollUpdateResultSet.java
@@ -2,36 +2,74 @@ package homework2;
 
 import java.sql.*;
 
-
 public class ScrollUpdateResultSet {
     public static void main(String[] args)
-        throws SQLException,ClassNotFoundException {
+            throws SQLException, ClassNotFoundException {
 
+        // بارگذاری درایور
         Class.forName("org.postgresql.Driver");
         System.out.println("Driver loaded");
 
-        Connection connection = DriverManager.getConnection
-                ("jdbc:postgresql://localhost:5432/javabook2","postgres","postgres");
+        // اتصال به دیتابیس
+        Connection connection = DriverManager.getConnection(
+                "jdbc:postgresql://localhost:5432/javabook",
+                "postgres", "postgres");
         connection.setAutoCommit(true);
         System.out.println("Database connected");
 
-        Statement statement = connection.createStatement
-                (ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+        // ساخت Statement قابل اسکرول و قابل ویرایش
+        Statement statement = connection.createStatement(
+                ResultSet.TYPE_SCROLL_INSENSITIVE,
+                ResultSet.CONCUR_UPDATABLE);
 
-        ResultSet resultSet = statement.executeQuery
-                ("select state, capital from StateCapital");
+        // اجرای کوئری
+        ResultSet resultSet = statement.executeQuery(
+                "SELECT state, capital FROM StateCapital");
 
-        System.out.println("Before update ");
+        System.out.println("Before update:");
         displayResultSet(resultSet);
 
+        // --- Update ---
+        if (resultSet.absolute(1)) { // رفتن به ردیف اول
+            resultSet.updateString("state", "New York");
+            resultSet.updateString("capital", "New D");
+            resultSet.updateRow();
+        }
+
+        // --- Insert ---
+        resultSet.moveToInsertRow();
+        resultSet.updateString("state", "Florida");
+        resultSet.updateString("capital", "Tallahassee");
+        resultSet.insertRow();
+        resultSet.moveToCurrentRow();
+
+        // --- Delete ---
+        if (resultSet.absolute(4)) { // فقط اگر ردیف چهارم وجود داشته باشد
+            resultSet.deleteRow();
+        }
+
+        // نمایش بعد از تغییرات
+        System.out.println("After update:");
+        resultSet = statement.executeQuery(
+                "SELECT state, capital FROM StateCapital");
+        displayResultSet(resultSet);
+
+        // بستن منابع
+        resultSet.close();
+        statement.close();
+        connection.close();
     }
 
-    public static void displayResultSet(ResultSet resultSet)throws SQLException {
-        ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+    // متد نمایش ResultSet
+    public static void displayResultSet(ResultSet resultSet)
+            throws SQLException {
+        ResultSetMetaData metaData = resultSet.getMetaData();
         resultSet.beforeFirst();
-        while (resultSet.next())
-            for (int i = 1; i <= resultSetMetaData.getColumnCount(); i++)
-                System.out.printf("%-12s\t",resultSet.getObject(i));
-        System.out.println();
+        while (resultSet.next()) {
+            for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                System.out.printf("%-12s\t", resultSet.getObject(i));
+            }
+            System.out.println();
+        }
     }
 }
