ee3a74db83f480e95fb1006692cebcffce52b8f9
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-12-16 08:56:30 -0800

    add ScrollUpdateResultSet

--- src/homework2/ScrollUpdateResultSet.java
+++ src/homework2/ScrollUpdateResultSet.java
@@ -18,6 +18,20 @@ public class ScrollUpdateResultSet {
         Statement statement = connection.createStatement
                 (ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
 
+        ResultSet resultSet = statement.executeQuery
+                ("select state, capital from StateCapital");
 
+        System.out.println("Before update ");
+        displayResultSet(resultSet);
+
+    }
+
+    public static void displayResultSet(ResultSet resultSet)throws SQLException {
+        ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+        resultSet.beforeFirst();
+        while (resultSet.next())
+            for (int i = 1; i <= resultSetMetaData.getColumnCount(); i++)
+                System.out.printf("%-12s\t",resultSet.getObject(i));
+        System.out.println();
     }
 }
